40cfc7700a73ee40dcb0b7cc5ab2b5c8c1fc3677
Lucas-megaleios <lucas-unifei@outlook.com>
2021-07-31 19:09:58 -0300

    Taking Photos

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
+import { FontAwesome } from "@expo/vector-icons";
 import { Camera } from "expo-camera";
-import React, { useEffect, useState } from "react";
-import { SafeAreaView, StyleSheet, View } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import { SafeAreaView, StyleSheet, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+  const camRef = useRef(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
   const [hasPermission, setHasPermission] = useState(null);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
 
   useEffect(() => {
     (async () => {
@@ -20,9 +23,35 @@ export default function App() {
     return <Text>No access to camera</Text>;
   }
 
+  async function takePicture() {
+    if (camRef) {
+      const data = await camRef.current.takePictureAsync();
+      setCapturedPhoto(data.uri);
+      console.log(data);
+    }
+  }
+
   return (
     <SafeAreaView style={styles.container}>
-      <Camera type={type} style={styles.camera}></Camera>
+      <Camera type={type} style={styles.camera} ref={camRef}>
+        <View style={styles.contentButtons}>
+          <TouchableOpacity
+            style={styles.buttonFlip}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            <FontAwesome name="exchange" size={24} color="red"></FontAwesome>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonCamera} onPress={takePicture}>
+            <FontAwesome name="camera" size={24} color="#FFFFFF"></FontAwesome>
+          </TouchableOpacity>
+        </View>
+      </Camera>
     </SafeAreaView>
   );
 }
@@ -36,5 +65,33 @@ const styles = StyleSheet.create({
     height: "100%",
     width: "100%",
   },
+  contentButtons: {
+    flex: 1,
+    backgroundColor: "transparent",
+    flexDirection: "row",
+  },
+  buttonFlip: {
+    position: "absolute",
+    bottom: 50,
+    left: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#FFFFFF",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
+  buttonCamera: {
+    position: "absolute",
+    bottom: 50,
+    right: 30,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "red",
+    margin: 20,
+    height: 50,
+    width: 50,
+    borderRadius: 50,
+  },
 });
-1;
